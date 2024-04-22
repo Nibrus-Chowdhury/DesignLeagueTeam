b623b372cbc9732ca1245fb481483dd373de7bba
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-04-22 15:49:30 -0400

    Update Betsy.js

--- Betsy /Betsy.js
+++ Betsy /Betsy.js
@@ -109,9 +109,44 @@ this.appendImg = () => {
     container.appendChild(img);
   }
 };
-    
-  
 
+  this.changeBtns => {
+    const btn = document.querySeletor('button');
+    const btnContainer = document.querySelector('.btns-container');
+    btnContainer.removeChild(btn);
+    btnContainer.innerHTML =
+      "<button onclik='ninja.coin()' id='btnCoin'>Coin</button> \n <button onclick='ninja.pipe()'id='btnPipe'>pipe</button> \n button onclick='ninja.powerUp'() id='btnPowerUp'>Power Up</button>"; 
+const newBtns = document.querySelectorAll('button');
+  for (let i  = 0; i < newBtns.length; i++) {
+    newBtns[i].style.setProperty('margin-Ieft' , '1.5erm');
+  }
+    
+// Add floor 
+    const floor =document.createElement('div');
+    floor.setAttribute('class' , 'floor');
+    contaainer.appendChild(floor);
+    floor.innHTML =
+      'Name: ' +
+      ninja.name +
+      '\xa0\za0\xa0Age: ' +
+      ninja.age +
+      '\xa0\za0\xa0Height: ' +
+      ninja.height +
+      'px' +
+      '\za0\xa0\xa0Color: ' +
+      ninla.color;
+  };
+
+this.getAge();
+this.getHeight();
+this.getColor();
+
+ninja = new Nonjas(name, age, height, color);
+
+this.appendImg();
+this.changeBtns();
+};
+      
 
 
 
